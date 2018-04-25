9afd9d04e2a07c77433e0d661edd508467176ce8
Gurpreet Singh <gurpreet@mailfence.com>
2018-04-24 20:54:55 -0500

    test new plugin functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,14 +10,54 @@ export class HomePage {
     constructor(public navCtrl: NavController) {
     }
 
-    testprint(){
-        alert("UI Testing!!~~");
-    }
-
     init() {
         window['cordova']['plugins']['ledger']['init'](
             (result) => {
                 console.log('Java code connected and result recieved')
+                alert(result);
+            },
+            (error) => {
+                alert(error);
+            }
+        );
+    }
+
+    setupWallet() {
+        window['cordova']['plugins']['ledger']['setupWallet'](
+            (result) => {
+                alert(result);
+            },
+            (error) => {
+                alert(error);
+            }
+        );
+    }
+
+    getWalletPublicAddress() {
+        window['cordova']['plugins']['ledger']['getWalletPublicAddress'](
+            (result) => {
+                alert(result);
+            },
+            (error) => {
+                alert(error);
+            }
+        );
+    }
+
+    verifyPin() {
+        window['cordova']['plugins']['ledger']['verifyPin'](
+            (result) => {
+                alert(result);
+            },
+            (error) => {
+                alert(error);
+            }
+        );
+    }
+
+    getPinRemainingAttempts() {
+        window['cordova']['plugins']['ledger']['getPinRemainingAttempts'](
+            (result) => {
                 alert(result);
             },
             (error) => {
