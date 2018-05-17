d841e151ffd3ab6950736905e6c390e8361a8185
Gurpreet Singh <gurpreet@mailfence.com>
2018-05-16 22:19:49 -0500

    started testing ledgerPlugin object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare var ledgerPlugin: any;
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,6 +19,17 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      var ledger = new ledgerPlugin();
+
+        ledger.init(  
+            (result) => {
+                console.log('Java code connected and result recieved')
+                alert(result);
+            },
+            (error) => {
+                alert(error);
+                console.log('VERY BAD')
+            });
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var ledgerPlugin: any;
+
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
 export class HomePage {
 
+    ledger: any;
+
     constructor(public navCtrl: NavController) {
     }
 
     init() {
-        window['cordova']['plugins']['ledger']['init'](
+        this.ledger = new ledgerPlugin();
+        this.ledger.init(
             (result) => {
                 console.log('Java code connected and result recieved')
                 alert(result);
             },
             (error) => {
                 alert(error);
+                console.log('VERY BAD')
             }
         );
     }
@@ -34,7 +40,7 @@ export class HomePage {
     }
 
     getWalletPublicAddress() {
-        window['cordova']['plugins']['ledger']['getWalletPublicAddress'](
+        window['cordova']['plugins']['ledger']['getWalletPublicKey'](
             (result) => {
                 alert(result);
             },
